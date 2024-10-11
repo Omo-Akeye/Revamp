8603c0619ce51fd4fdf79f5bfa090517c6c89b2e
omo-Akeye <akeyesaheed@gmail.com>
2024-10-11 21:10:30 +0100

    implement with bime changes

--- src/app/home/ui/Why.tsx
+++ src/app/home/ui/Why.tsx
@@ -1,41 +1,37 @@
-import lonely from '../../../../public/images/lonely-3d-render-icon-illustration-png 1.png'
-import growth from '../../../../public/images/growth-graph-banner.png'
-import Image from 'next/image'
+
+
 export default function Why() {
   return (
-    <div className='md:mt-24 mt-14'>
-       
-       <div className='md:w-[40%] w-[60%] mx-auto text-center md:mb-24 mb-[72px]'>
-         
-       <p className='text-[#0E0F0F99] text-opacity-60 uppercase max-md:text-xs text-xl mb-3 font-medium'>WHO SAID DATA MUST BE MESSY?</p>
-        <p className='md:text-2xl font-semibold text-sm max-md:mt-[6px]'>As your business starts to grow you need a system that can help in accelerating its growth</p>
-       </div>
+    <div className='md:mt-16 mt-14'>
+      
         <div className='flex gap-x-10 w-[85%] mx-auto max-lg:gap-y-8 justify-center max-lg:flex-col max-lg:items-center'>
-            <section className="bg-[#F7F7F7] rounded-[20px]   max-w-[569px] md:pl-12 pl-[27px] md:pr-20 pr-5 md:py-14 py-10 w-full">
-                <h1 className='md:text-3xl text-2xl font-semibold mb-5'>Without Bime</h1>
+            <section className="max-w-[569px] w-full flex justify-between flex-col">
+            <div className='py-10 bg-[#F7F7F7] px-8  rounded-[20px] mb-5 relative'>
+              <img src="images/idea-3d.png" alt="idea" className="absolute right-[5%] top-[5%]" />
+            <p className='max-md:text-xs text-xl mb-3 font-medium'>Who said data must be messy?</p>
+            <p className='md:text-xl  text-sm max-md:mt-[6px] '>As your business starts to grow you need a system that can help in accelerating its growth</p>
+            </div>
+            <div className='bg-[#F7F7F7] rounded-[20px] pt-[72px] pb-[52px] pl-10'>
+            <h1 className='md:text-[32px] text-2xl font-semibold mb-5'>Without Bime</h1>
                 <ul className="md:text-xl md:gap-y-3 gap-4 list-inside list-disc">
                     <li>Fraustrating messy data takes forever to get refined</li>
                     <li>Scaling seem impossible at the absence of accurate data</li>
                     <li>Delay in service delivery to customers</li>
                 </ul>
-                <div className='w-full md:mt-28 mt-14 justify-end flex'>
-                <Image src={lonely} alt="img"  />
-                </div>
+            </div>
+              
                
             </section>
-            <section className="bg-bime-blue rounded-[20px] max-w-[569px] md:pl-12 pl-[27px]  pr-10 md:py-14 py-10 w-full">
+            <section className="rounded-[20px] max-w-[569px] pt-8 w-full border-[2px] border-[#F5F5F5] px-10 pb-12">
+              <img src="images/free-bar.png" alt="bar" />
   <h1 className="text-3xl font-semibold mb-5">With Bime</h1>
   <li className="md:text-xl list-inside list-disc">
     Your business finances are properly managed and crystal clear, giving you more time to grow and potentially scale your business
   </li>
-  
-  <div className="inline-flex max-md:flex-col md:items-end items-start mt-5">
-    <Image src={growth} alt="growth" />
-
-    <button className="md:px-10 px-6 text-sm font-medium bg-white py-[10px] rounded-[10px] inline-block text-nowrap max-md:mt-6">
-      Get started
-    </button>
-  </div>
+  <div className="flex text-[#329ACF] font-semibold gap-x-[10px] mt-6 hover:gap-x-4 cursor-pointer transition items-center">
+              <p>Get started</p>
+              <img src= "/images/arrow_forward.png" alt="" width={18}/>
+            </div>
 </section>
 
         </div>
