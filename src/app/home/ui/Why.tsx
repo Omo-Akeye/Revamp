3118c8a26965ccdd1695c8eb12d3822556ccea32
omo-Akeye <akeyesaheed@gmail.com>
2024-10-11 21:23:48 +0100

    text resized

--- src/app/home/ui/Why.tsx
+++ src/app/home/ui/Why.tsx
@@ -9,11 +9,11 @@ export default function Why() {
             <div className='py-10 bg-[#F7F7F7] px-8  rounded-[20px] mb-5 relative'>
               <img src="images/idea-3d.png" alt="idea" className="absolute right-[5%] top-[5%]" />
             <p className='max-md:text-xs text-xl mb-3 font-medium'>Who said data must be messy?</p>
-            <p className='md:text-xl  text-sm max-md:mt-[6px] '>As your business starts to grow you need a system that can help in accelerating its growth</p>
+            <p className='md:text-lg  text-sm max-md:mt-[6px] '>As your business starts to grow you need a system that can help in accelerating its growth</p>
             </div>
             <div className='bg-[#F7F7F7] rounded-[20px] pt-[72px] pb-[52px] pl-10'>
             <h1 className='md:text-[32px] text-2xl font-semibold mb-5'>Without Bime</h1>
-                <ul className="md:text-xl md:gap-y-3 gap-4 list-inside list-disc">
+                <ul className="md:text-lg md:gap-y-3 gap-4 list-inside list-disc">
                     <li>Fraustrating messy data takes forever to get refined</li>
                     <li>Scaling seem impossible at the absence of accurate data</li>
                     <li>Delay in service delivery to customers</li>
@@ -25,7 +25,7 @@ export default function Why() {
             <section className="rounded-[20px] max-w-[569px] pt-8 w-full border-[2px] border-[#F5F5F5] px-10 pb-12">
               <img src="images/free-bar.png" alt="bar" />
   <h1 className="text-3xl font-semibold mb-5">With Bime</h1>
-  <li className="md:text-xl list-inside list-disc">
+  <li className="md:text-lg list-inside list-disc">
     Your business finances are properly managed and crystal clear, giving you more time to grow and potentially scale your business
   </li>
   <div className="flex text-[#329ACF] font-semibold gap-x-[10px] mt-6 hover:gap-x-4 cursor-pointer transition items-center">
